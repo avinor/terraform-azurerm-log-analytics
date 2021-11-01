ceefb4db3282bc8bce6987934174e3c9b5a49403
Øyvind Strømmen <insula.ventus@gmail.com>
2021-11-01 13:23:08 +0100

    Run terraform fmt

--- examples/simple-workspace/main.tf
+++ examples/simple-workspace/main.tf
@@ -1,7 +1,7 @@
 module "workspace" {
-    source = "../../"
+  source = "../../"
 
-    name = "simple"
-    resource_group_name = "simple-rg"
-    location = "westeurope"
+  name                = "simple"
+  resource_group_name = "simple-rg"
+  location            = "westeurope"
 }
